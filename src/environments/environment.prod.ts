a7ad9570a63b5e1fc68e7daf61e2e580381f0b74
chetan <ladumorchetan77@gmail.com>
2018-09-28 19:19:26 +0530

    Environment added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDdOy1Lx4wZKnqeUsq7-_QzZ2DrYkZw_po",
+    authDomain: "fir-demo-9eca5.firebaseapp.com",
+    databaseURL: "https://fir-demo-9eca5.firebaseio.com",
+    projectId: "fir-demo-9eca5",
+    storageBucket: "fir-demo-9eca5.appspot.com",
+    messagingSenderId: "465566249581"
+  }
 };
